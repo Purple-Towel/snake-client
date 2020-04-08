339122568a9ef019095fe00117d48a73688ff99b
Angad Grewal <angadgrewal31@gmail.com>
2020-04-08 23:04:05 +0000

    added test code for movement and commented it out

--- client.js
+++ client.js
@@ -15,7 +15,17 @@ const connect = function() {
   // sends name to server
   conn.on('connect', () => {
     conn.write("Name: P-T")
-  })
+  });
+  /*
+  conn.on('connect', () => {
+    conn.write("Move: up")
+  });
+
+  conn.on('connect', () => {
+    setInterval(() => {
+      conn.write("Move: left")}, 1000)
+  });
+  */
   // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('data', (data) => {
